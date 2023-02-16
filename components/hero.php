b5eabc3bfb72e358c2b684137de782499527cc5b
Albina0304 <aaa.aniskevich@gmail.com>
2023-02-16 12:52:09 +0100

    test

---
+++ components/hero.php
@@ -0,0 +1,51 @@
+<section class=" section hero">
+            <div class="container">
+                <div class="hero-global">
+                    <div class="hero-colon-left">
+                        <div class="hero-title">
+                            <h1>We Design Impactful Digital 
+                                <span class="hero-products">
+                                    <a href="#" class="hero-products-link"> Products</a>
+                                </span>
+                                <span class="hero-background"></span>
+                            </h1>
+                        </div>
+                        <div class="hero-text">
+                            <p>
+                                Help find solutions with intitutive and in accordance with client business goals. we provide a high-quality services.
+                            </p>
+                        </div>
+                        <div class="section-button-wrapper">
+                            <div class="section-button">
+                                <a class="btn btn-secondary" href="#">Contact Us</a>
+                            </div>
+                            <div class="section-button">
+                                <a class="btn btn-primary" href="#"> <img src="wp-content/themes/power/assets/images/Button.png"> Watch our
+                                    introduction video</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hero-colon-right">
+                        <div class="hero-colon-right-images">
+                            <div class="hero-img-bg">
+                                <img src="wp-content/themes/power/assets/images/background-girl.png">
+                            </div>
+                            <div class="hero-img-global">
+                                <img src="wp-content/themes/power/assets/images/girl.png">
+                            </div>
+                            <!-- <div class="hero-img-dependent"> -->
+                                <div class="hero-img-first">
+                                    <img src="wp-content/themes/power/assets/images/xd.png">
+                                </div>
+                                <div class="hero-img-second">
+                                    <img src="wp-content/themes/power/assets/images/figma.png">
+                                </div>
+                                <div class="hero-img-last">
+                                    <img src="wp-content/themes/power/assets/images/diamond.png">
+                                </div>
+                            <!-- </div> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
---
+++ footer.php
@@ -0,0 +1,4 @@
+<script src="wp-content/themes/power//assets/js/main.js"></script>
+<?php wp_footer()?>
+    </body>
+</html>
